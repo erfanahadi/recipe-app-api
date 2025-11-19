0493d2c2aaa6ad35c63d93a609c20c6a40e8137a
erfanahadi <erfan_ahadi@ymail.com>
2025-11-19 12:34:22 +0330

    fix linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,11 +6,7 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
-        res = calc.subtract(7,3)
+        res = calc.subtract(7, 3)
         self.assertEqual(res, 4)
-        
-        
-        
-        
