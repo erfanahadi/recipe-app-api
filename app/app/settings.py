31f61ed11cdb089e8ab855c4f3d8db2b2ede646d
erfanahadi <erfan_ahadi@ymail.com>
2025-11-30 15:23:44 +0330

    final touchs

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5&8pjwlg%v^tg@pfo$=v8i^u_jy&wpak2$!7@r0!ft74fd1p7t"
-
+SECRET_KEY = os.environ.get('SECRET_KEY','changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(None,
+           os.environ.get('ALLOWED_HOSTS', '').split(','))
+    )
 
 
 # Application definition
--- proxy/run.sh
+++ proxy/run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
 set -e
 
-envsubst < /etc/nginx/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
